fadaae64192c676478d52f579a61875e0273e34e
Adam Arsanukaev <gtoressan@gmail.com>
2020-02-09 23:50:42 +0300

    Change functions parameters

--- src/coututil.rs
+++ src/coututil.rs
@@ -1,4 +1,4 @@
-pub fn print_vector(vector: &Vec<i32>, columns: i32) {
+pub fn print_vector(vector: &Vec<i32>, columns: u32) {
     let mut index = 0;
 
     for i in vector {
@@ -18,15 +18,15 @@ pub fn print_vector_len(vector: &Vec<i32>) {
     println!("Vector length: {}", vector.len());
 }
 
-pub fn print_memory_access(count: i32) {
+pub fn print_memory_access(count: u32) {
     println!("Memory accesses count: {}", count);
 }
 
-pub fn print_comparisons(count: i32) {
+pub fn print_comparisons(count: u32) {
     println!("Comparisons count: {}", count);
 }
 
-pub fn print_elapsed_time(time: i32) {
+pub fn print_elapsed_time(time: u32) {
     println!("Time in milliseconds: {}", time);
 }
 
