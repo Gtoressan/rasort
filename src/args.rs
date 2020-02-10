21cad952e535cbbc41946badf4ac411cc76a736b
Adam Arsanukaev <gtoressan@gmail.com>
2020-02-10 21:11:40 +0300

    Add fields

--- src/args.rs
+++ src/args.rs
@@ -1,6 +1,8 @@
 pub struct State {
     pub print_unsorted_vector: bool,
+    pub unsorted_vector_columns: u128,
     pub print_sorted_vector: bool,
+    pub sorted_vector_columns: u128,
     pub print_comparisons: bool,
     pub print_memory_accesses: bool,
     pub print_elapsed_time: bool,
@@ -8,8 +10,7 @@ pub struct State {
 }
 
 pub struct StateValue {
-    pub unsorted_vector: Vec<i32>,
-    pub sorted_vector: Vec<i32>,
+    pub vector: Vec<i32>,
     pub comparisons: u128,
     pub memory_accesses: u128,
 
