52080ba0a2d5b1029675ad690db07d17606efc7d
Adam Arsanukaev <gtoressan@gmail.com>
2020-02-10 20:56:06 +0300

    Add program state structure

--- src/args.rs
+++ src/args.rs
@@ -1,11 +1,18 @@
-// TODO: add meaningful names to fields
-pub struct Config {
-    pub UNSORTED_VECTOR: bool,
-    pub UNSORTED_VECTOR_COLUMNS: u32,
-    pub SORTED_VECTOR: bool,
-    pub SORTED_VECTOR_COLUMNS: u32,
-    pub COMPARISONS: bool,
-    pub MEMORY_ACCESSES: bool,
-    pub TIME: bool,
-    pub VECTOR_LEN: bool,
+pub struct State {
+    pub print_unsorted_vector: bool,
+    pub print_sorted_vector: bool,
+    pub print_comparisons: bool,
+    pub print_memory_accesses: bool,
+    pub print_elapsed_time: bool,
+    pub print_vector_length: bool,
+}
+
+pub struct StateValue {
+    pub unsorted_vector: Vec<i32>,
+    pub sorted_vector: Vec<i32>,
+    pub comparisons: u128,
+    pub memory_accesses: u128,
+
+    // Time counts in milliseconds.
+    pub elapsed_time: u128,
 }
