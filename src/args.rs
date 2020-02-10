19f1ca1bc6b5a7aa281f6b0f358831b89e9b2e99
Sergey Petrov <serridd@gmail.com>
2020-02-10 20:21:05 +0300

    Move global var configurations into Config

---
+++ src/args.rs
@@ -0,0 +1,11 @@
+// TODO: add meaningful names to fields
+pub struct Config {
+    pub UNSORTED_VECTOR: bool,
+    pub UNSORTED_VECTOR_COLUMNS: u32,
+    pub SORTED_VECTOR: bool,
+    pub SORTED_VECTOR_COLUMNS: u32,
+    pub COMPARISONS: bool,
+    pub MEMORY_ACCESSES: bool,
+    pub TIME: bool,
+    pub VECTOR_LEN: bool,
+}
